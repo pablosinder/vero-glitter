b1ce5f90c53658efbc476e5e2cc58c6ca12f348b
pablosinder <74592657+pablosinder@users.noreply.github.com>
2025-10-23 09:48:17 +0000

    Updates

---
+++ controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["./BaseController"],function(t){"use strict";return t.extend("com.veroglitter.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
+//# sourceMappingURL=App.controller.js.map
---
+++ test/unit/unitTests.qunit.js
@@ -0,0 +1,2 @@
+sap.ui.define(["./controller/Main.qunit"]);
+//# sourceMappingURL=unitTests.qunit.js.map
